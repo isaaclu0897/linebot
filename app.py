ce63894d3e90f2dcd78b9085114cc7cc154866ae
wei <t104306033@ntut.org.tw>
2018-12-04 19:56:13 +0800

    /test2run: clean

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask, request, abort
+# -*- coding: utf-8 -*-
 
-from linebot import (
-    LineBotApi, WebhookHandler
-)
-from linebot.exceptions import (
-    InvalidSignatureError
-)
+import os
+
+from flask import Flask, abort, request
+
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
 from linebot.models import *
 
 app = Flask(__name__)
@@ -36,7 +36,6 @@ def handle_message(event):
     message = TextSendMessage(text=event.message.text)
     line_bot_api.reply_message(event.reply_token, message)
 
-import os
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
