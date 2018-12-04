2e10e5f3ec9ef042d6af66c65b8f638e036d0291
wei <t104306033@ntut.org.tw>
2018-12-04 21:02:47 +0800

    A

--- app.py
+++ app.py
@@ -33,8 +33,9 @@ def callback():
 # 處理訊息
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    message = TextSendMessage(text=event.message.text)
-    line_bot_api.reply_message(event.reply_token, "姚韋辰很帥")
+    # message = TextSendMessage(text="event.message.text")
+    message = TextSendMessage(text="姚韋辰很帥")
+    line_bot_api.reply_message(event.reply_token, message)
 
 import os
 if __name__ == "__main__":
