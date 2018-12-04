45fd4a2623b22e69d01562b5f974c05e82d5629c
wei <t104306033@ntut.org.tw>
2018-12-04 21:10:22 +0800

    aaa

--- app.py
+++ app.py
@@ -33,6 +33,7 @@ def callback():
 # 處理訊息
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+    print(event.)
     # message = TextSendMessage(text="event.message.text")
     message = TextSendMessage(text="姚韋辰很帥")
     line_bot_api.reply_message(event.reply_token, message)
