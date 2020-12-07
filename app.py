1bbe28b499b78529adc8014a3762602a0e9d892e
wei <t104306033@ntut.org.tw>
2020-12-08 01:25:28 +0800

    echo linebot

--- app.py
+++ app.py
@@ -46,8 +46,7 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     ''' 處理訊息 '''
-    print(event.message.text)
-    message = TextSendMessage(text=f"event.message.text{event.message.text}")
+    message = TextSendMessage(text=f"{event.message.text}")
     line_bot_api.reply_message(event.reply_token, message)
 
 
